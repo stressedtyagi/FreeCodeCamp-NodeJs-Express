c09e11febc6b024ddafe30dabc4bf1adb6f0bb8b
creatorgaming <divyanshutyagi8@gmail.com>
2021-11-27 22:51:40 +0530

    Added server side rendering for filtering content, fully working get request done

--- 04-store-api/controllers/products.js
+++ 04-store-api/controllers/products.js
@@ -1,9 +1,72 @@
+const Products = require("../models/product");
 const getAllProductsStatic = async (req, res) => {
-    res.status(200).json({ msg: "product testing route" });
+    const products = await Products.find({}).select("name price");
+    res.status(200).json({ products });
 };
 
 const getAllProducts = async (req, res) => {
-    res.status(200).json({ msg: "product route" });
+    const { featured, company, name, sort, fields, numericFilters } = req.query;
+    const queryObject = {};
+
+    if (featured) {
+        queryObject.featured = featured === true ? true : false;
+    }
+
+    if (company) {
+        queryObject.company = company;
+    }
+
+    if (name) {
+        queryObject.name = { $regex: name, $options: "" };
+    }
+
+    // Numeric Filters - for price and rating
+    // These filters will come in get request as follows :
+    // price>30,rating<=3
+    if (numericFilters) {
+        const operatorMap = {
+            ">": "$gt",
+            ">=": "$gte",
+            "=": "$eq",
+            "<": "$lt",
+            "<=": "$lte",
+        };
+        const regEx = /\b(<|>|>=|=|<=)\b/g;
+        let filters = numericFilters.replace(
+            regEx,
+            (match) => `-${operatorMap[match]}-`
+        );
+        const options = ["price", "rating"];
+        filters = filters.split(",").forEach((item) => {
+            const [field, operator, value] = item.split("-");
+            if (options.includes(field)) {
+                queryObject[field] = { [operator]: Number(value) };
+            }
+        });
+    }
+    console.log(queryObject);
+    let result = Products.find(queryObject);
+
+    if (sort) {
+        const sortList = sort.split(",").join(" ");
+        result = result.sort(sortList);
+    } else {
+        result = result.sort("createdAt");
+    }
+
+    if (fields) {
+        const fieldsList = fields.split(",").join(" ");
+        result = result.select(fieldsList);
+    }
+
+    // For pagination and displaying specific number of items on 1 page
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+    result = result.skip(skip).limit(limit);
+
+    const products = await result;
+    res.status(200).json({ nbHits: products.length, products });
 };
 
 module.exports = {
