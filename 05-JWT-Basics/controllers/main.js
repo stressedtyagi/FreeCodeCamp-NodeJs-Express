ce1691f3bfd0985acfb301025f055b19fb1dcc04
creatorgaming <divyanshutyagi8@gmail.com>
2021-12-18 18:39:32 +0530

    login route jwt work done

--- 05-JWT-Basics/controllers/main.js
+++ 05-JWT-Basics/controllers/main.js
@@ -6,13 +6,35 @@
 
         - Finally, setup authentication so only the request with JWT can access the dashboard
 */
+const CustomAPIError = require("../errors/custom-error");
+const jwt = require("jsonwebtoken");
 
 const login = async (req, res) => {
-    res.status(200).send("LOGIN ROUTE");
+    const { username, password } = req.body;
+    /* 
+        We can use 3 ways to do validation :
+            1. Checking up on database using mongoose validation (not doing this in this project)
+            2. JOI package
+            3. check in with controller
+    */
+    if (!username || !password) {
+        throw new CustomAPIError("Username or Password not found", 400);
+    }
+
+    // Demo Payload, ideal way is to connect to DB and send the user id as payload
+    // so that later we can retrieve that easily
+    const id = new Date().getDate();
+    // Keep payload small, for better user experience. Bigger payload more time to decode
+    const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+        expiresIn: "30d",
+    });
+
+    res.status(200).json({ msg: "User Created", token });
 };
 
 const dashboard = async (req, res) => {
     const luckyNumber = Math.floor(Math.random() * 100);
+    console.log(req.headers);
     res.status(200).json({
         msg: "Hello Sir",
         secret: `Here is your authorized data, lucky number ${luckyNumber}`,
