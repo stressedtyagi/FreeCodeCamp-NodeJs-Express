a896db8037e72e13febaaaa730bf8ab2e545b668
creatorgaming <divyanshutyagi8@gmail.com>
2021-12-26 18:29:32 +0530

    refactored error handling for validation,server and cast errors

--- 06-jobs-api/middleware/error-handler.js
+++ 06-jobs-api/middleware/error-handler.js
@@ -1,11 +1,50 @@
-const { CustomAPIError } = require("../errors");
+// const { CustomAPIError } = require("../errors");
 const { StatusCodes } = require("http-status-codes");
+
 const errorHandlerMiddleware = (err, req, res, next) => {
-    if (err instanceof CustomAPIError) {
-        return res.status(err.statusCode).json({ msg: err.message });
+    /*  
+        doing this `customError` functionality for those error that does not 
+        come into our custom errors or anyother error
+        we classify them into as INTERNAL_SERVER_ERROR
+    */
+    let customError = {
+        // setDefault statuscode
+        statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+        msg: err.message || "Something went wrong try again later",
+    };
+
+    /* 
+        Now we don't need to do this for custom error since we are checking it while making the 
+        custom error object above
+
+        if (err instanceof CustomAPIError) {
+            return res.status(err.statusCode).json({ msg: err.message });
+        }
+    */
+
+    // Managing the cast error, for example if we messup with the `id` in the URL while making updateJob routes
+    if (err.name === "CastError") {
+        customError.msg = `No item found with id : ${err.value._id}`;
+        customError.statusCode = 404;
     }
-    console.log(err);
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+
+    // This is for validation error
+    if (err.name === "ValidationError") {
+        customError.msg = Object.values(err.errors)
+            .map((item) => item.message)
+            .join(",");
+        customError.statusCode = 400;
+    }
+
+    // This is for when we try to register a user with already existing email id
+    if (err.code || err.code == 1100) {
+        customError.msg = `Duplicate value entered for ${Object.keys(
+            err.keyValue
+        )} field, please chose another value`;
+        customError.statusCode = 400;
+    }
+    // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+    return res.status(customError.statusCode).json({ msg: customError.msg });
 };
 
 module.exports = errorHandlerMiddleware;
